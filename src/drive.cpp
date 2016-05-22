842157a831f4540a91a3e7e0b9c95a8d839840ab
Elton Viana <eltonvs@outlook.com>
2016-05-22 12:24:00 -0300

    Added initial files

---
+++ src/drive.cpp
@@ -0,0 +1,20 @@
+/*!
+ *  @mainpage GREMLINS
+ *  @author Elton de Souza Vieira
+ *  @date May, 2016
+ *  @copyright Copyright &copy; 2016. All rights reserved.
+ *  @version 1.0
+ *
+ *  @file drive.cpp
+ *  @brief Driver File
+ *
+ *  File with the Memory Manager tests
+ */
+
+#include <iostream>
+
+int main(int argc, char const *argv[]) {
+    std::cout << ">>> Exiting with success...\n";
+
+    return EXIT_SUCCESS;
+}
